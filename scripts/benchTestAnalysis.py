9fff53e9c4e7d376a34521eeeae0ae66cec0471b
cameronbrooks11 <cambrooks3393@gmail.com>
2024-04-19 22:38:59 -0400

    formatted all python with black formatter

--- scripts/benchTestAnalysis.py
+++ scripts/benchTestAnalysis.py
@@ -18,7 +18,7 @@ Created on Mon Aug  9 07:47:06 2021
     9)  Check expected GPS update rate
     10) Ensure the test time matches the expected length
  """
- 
+
 """-------------------------------
 IMPORT DATA FILE
 -------------------------------"""
@@ -27,178 +27,190 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import win32gui, win32con, os
 
-plt.rcParams['savefig.dpi'] = 300
+plt.rcParams["savefig.dpi"] = 300
 plt.rcParams["figure.dpi"] = 300
 
-filter='Data Files\0*.txt;\0'
-customfilter='*.txt'
-path, customfilter, flags=win32gui.GetOpenFileNameW(
-    InitialDir=os.environ['temp'],
-    Flags=win32con.OFN_ALLOWMULTISELECT|win32con.OFN_EXPLORER,
-    #File='somefilename', 
-    DefExt='txt',
-    Title='Select Rocket Data File',
+filter = "Data Files\0*.txt;\0"
+customfilter = "*.txt"
+path, customfilter, flags = win32gui.GetOpenFileNameW(
+    InitialDir=os.environ["temp"],
+    Flags=win32con.OFN_ALLOWMULTISELECT | win32con.OFN_EXPLORER,
+    # File='somefilename',
+    DefExt="txt",
+    Title="Select Rocket Data File",
     Filter=filter,
     CustomFilter=customfilter,
-    FilterIndex=0)
+    FilterIndex=0,
+)
 
 df = pd.read_csv(
-    path, 
-    dtype={'fltEvents': str}, 
-    engine='python', 
-    delimiter = ",", 
-    skiprows = 0, 
-    skipfooter= 8, 
-    index_col = False)
+    path,
+    dtype={"fltEvents": str},
+    engine="python",
+    delimiter=",",
+    skiprows=0,
+    skipfooter=8,
+    index_col=False,
+)
 
 df.columns.values[0] = "timeStamp"
-df['flightTime']= df['timeStamp']/1000000
+df["flightTime"] = df["timeStamp"] / 1000000
 
 """-------------------------------
 SET ACCELEROMETER GAINS
 -------------------------------"""
 teensyGen = 3.2
 if teensyGen == 1:
-    accelGain = 0.012 #LSM303
-    gainHighG = 0.0183 #ADXL377 & ADS1115 Combo
-    centerPoint = 13125 #ADXL377 & ADS1115 Combo
+    accelGain = 0.012  # LSM303
+    gainHighG = 0.0183  # ADXL377 & ADS1115 Combo
+    centerPoint = 13125  # ADXL377 & ADS1115 Combo
     centerPoint = 0
 elif teensyGen == 3:
-    accelGain = 0.000732 #LSM9DS1
-    gainHighG = 1/129 #ADXL377 and Teensy ADC
+    accelGain = 0.000732  # LSM9DS1
+    gainHighG = 1 / 129  # ADXL377 and Teensy ADC
     highGoffset = 1
     centerPoint = 0
 elif teensyGen == 3.1:
-    accelGain = 0.012 #LSM303
-    gainHighG = 1/129 #ADXL377 and Teensy ADC
+    accelGain = 0.012  # LSM303
+    gainHighG = 1 / 129  # ADXL377 and Teensy ADC
     centerPoint = 0
 elif teensyGen == 3.2 or teensyGen == 3.3:
-    accelGain = 0.000732 #LSM9DS1
+    accelGain = 0.000732  # LSM9DS1
     centerPoint = 0
-    gainHighG = 0.049 #H3LIS331DL
+    gainHighG = 0.049  # H3LIS331DL
 
-#Calculate the cycle times    
-dataRate = df.loc[:,'timeStamp'].index.max()/15
+# Calculate the cycle times
+dataRate = df.loc[:, "timeStamp"].index.max() / 15
 for i in range(df.timeStamp.index.max()):
-    
-    if i == 0: df.at[i, 'dt'] = 0
-    else: df.at[i, 'dt'] = df.at[i, 'timeStamp'] - df.at[i-1, 'timeStamp']
 
-#plot the cycle times
-df['dt'].plot(
-    x = 'flightTime',
-    xlabel = "Flight Time",
-    y = 'dt',
-    ylabel = "Cycle Time",
-    title = 'Samples per Second: ' + str(dataRate)[0:4])
+    if i == 0:
+        df.at[i, "dt"] = 0
+    else:
+        df.at[i, "dt"] = df.at[i, "timeStamp"] - df.at[i - 1, "timeStamp"]
 
-#plot the accelerometers
-df['accelZ']*= accelGain
-df['smoothHighGz']*= gainHighG
-df['highGz']*= gainHighG
+# plot the cycle times
+df["dt"].plot(
+    x="flightTime",
+    xlabel="Flight Time",
+    y="dt",
+    ylabel="Cycle Time",
+    title="Samples per Second: " + str(dataRate)[0:4],
+)
+
+# plot the accelerometers
+df["accelZ"] *= accelGain
+df["smoothHighGz"] *= gainHighG
+df["highGz"] *= gainHighG
 df.plot(
-    x = 'flightTime',
-    xlabel = "Flight Time",
-    y = ['highGz','smoothHighGz','accelZ'],
-    ylabel = "G-load",
-    title = 'Acceleration Data')
+    x="flightTime",
+    xlabel="Flight Time",
+    y=["highGz", "smoothHighGz", "accelZ"],
+    ylabel="G-load",
+    title="Acceleration Data",
+)
 
-#look at the first second of data
+# look at the first second of data
 df.head(1000).plot(
-    x = 'flightTime',
-    xlabel = "Flight Time",
-    y = ['smoothHighGz','accelZ'],
-    ylabel = "m/s^2",
-    title = 'Accleration Data')
+    x="flightTime",
+    xlabel="Flight Time",
+    y=["smoothHighGz", "accelZ"],
+    ylabel="m/s^2",
+    title="Accleration Data",
+)
 
-#plot rotation
-df['pitchX']*=0.1
-df['yawY']*=0.1
-df['offVert']*=0.1
+# plot rotation
+df["pitchX"] *= 0.1
+df["yawY"] *= 0.1
+df["offVert"] *= 0.1
 df.plot(
-        x = 'flightTime',
-        xlabel = "Flight Time",
-        y = ['rollZ', 'yawY', 'pitchX','offVert'],
-        ylabel = "Degrees",
-        title = 'Rotation Data')
+    x="flightTime",
+    xlabel="Flight Time",
+    y=["rollZ", "yawY", "pitchX", "offVert"],
+    ylabel="Degrees",
+    title="Rotation Data",
+)
 
-#plot gyro data
+# plot gyro data
 df.plot(
-        x = 'flightTime',
-        xlabel = 'Flight Time',
-        y = ['gyroX', 'gyroY', 'gyroZ'],
-        ylabel = 'degrees per second',
-        title = 'Gyro Output Data')
+    x="flightTime",
+    xlabel="Flight Time",
+    y=["gyroX", "gyroY", "gyroZ"],
+    ylabel="degrees per second",
+    title="Gyro Output Data",
+)
 
-#plot baro temp
-df.fillna(method='pad').plot(
-    x = 'flightTime',
-    xlabel = 'Flight Time',
-    y = 'baroTemp',
-    ylabel = 'deg C',
-    title = 'Barometric Temperature')
+# plot baro temp
+df.fillna(method="pad").plot(
+    x="flightTime",
+    xlabel="Flight Time",
+    y="baroTemp",
+    ylabel="deg C",
+    title="Barometric Temperature",
+)
 
-#plot baro data
-df.fillna(method='pad').plot(
-    x = 'flightTime',
-    xlabel = 'Flight Time',
-    y = ['baroAlt','altMoveAvg'],
-    ylabel = 'meters',
-    title = 'Barometric Altitude')
+# plot baro data
+df.fillna(method="pad").plot(
+    x="flightTime",
+    xlabel="Flight Time",
+    y=["baroAlt", "altMoveAvg"],
+    ylabel="meters",
+    title="Barometric Altitude",
+)
 
-#plot baro velocity
-df.fillna(method='pad').plot(
-    x = 'flightTime',
-    xlabel = 'Flight Time',
-    y = ['baroVel'],
-    ylabel = 'Baro Velocity',
-    title = 'Barometric Velocity')
+# plot baro velocity
+df.fillna(method="pad").plot(
+    x="flightTime",
+    xlabel="Flight Time",
+    y=["baroVel"],
+    ylabel="Baro Velocity",
+    title="Barometric Velocity",
+)
 
-#plot integrated velocity
-df.fillna(method='pad').plot(
-    x = 'flightTime',
-    xlabel = 'Flight Time',
-    y = 'intVel',
-    ylabel = 'meter per second',
-    title = 'IMU Velocity')
+# plot integrated velocity
+df.fillna(method="pad").plot(
+    x="flightTime",
+    xlabel="Flight Time",
+    y="intVel",
+    ylabel="meter per second",
+    title="IMU Velocity",
+)
 
-#plot integrated altitude
-df.fillna(method='pad').plot(
-    x = 'flightTime',
-    xlabel = 'Flight Time',
-    y = 'intAlt',
-    ylabel = 'meters',
-    title = 'IMU Altitude')
+# plot integrated altitude
+df.fillna(method="pad").plot(
+    x="flightTime",
+    xlabel="Flight Time",
+    y="intAlt",
+    ylabel="meters",
+    title="IMU Altitude",
+)
 
-#plot fusion velocity
-df.fillna(method='pad').plot(
-    x = 'flightTime',
-    xlabel = 'Flight Time',
-    y = 'fusionVel',
-    ylabel = 'meter per second',
-    title = 'Fusion Velocity')
+# plot fusion velocity
+df.fillna(method="pad").plot(
+    x="flightTime",
+    xlabel="Flight Time",
+    y="fusionVel",
+    ylabel="meter per second",
+    title="Fusion Velocity",
+)
 
-#plot fusion velocity
-df.fillna(method='pad').plot(
-    x = 'flightTime',
-    xlabel = 'Flight Time',
-    y = 'fusionAlt',
-    ylabel = 'meter per second',
-    title = 'Fusion Altitude')
+# plot fusion velocity
+df.fillna(method="pad").plot(
+    x="flightTime",
+    xlabel="Flight Time",
+    y="fusionAlt",
+    ylabel="meter per second",
+    title="Fusion Altitude",
+)
 
-#plot gps packets
-df.fillna(method='pad').plot(
-        y='gnssSatellites',
-        x='flightTime',
-        title = 'GNSS Coords RXd')
+# plot gps packets
+df.fillna(method="pad").plot(
+    y="gnssSatellites", x="flightTime", title="GNSS Coords RXd"
+)
 
-#plot telemetry packets
+# plot telemetry packets
 for i in range(df.timeStamp.index.max()):
-    if pd.isna(df.at[i, 'radioPacketNum']): df.at[i, 'packetNum']=0
-    else: df.at[i, 'packetNum']=1
-df.plot(
-        y='packetNum',
-        x='flightTime',
-        title = 'Packets Sent',
-        legend = False)
-
+    if pd.isna(df.at[i, "radioPacketNum"]):
+        df.at[i, "packetNum"] = 0
+    else:
+        df.at[i, "packetNum"] = 1
+df.plot(y="packetNum", x="flightTime", title="Packets Sent", legend=False)
